ec9d2f31ab9773f4c177f65690345d65bbc7cba9
XingLingQAQ <XingLingQAQ@users.noreply.github.com>
2024-07-02 13:16:37 +0000

    deploy: a0f22b9e0ed0be0419d097e64a20523406429d01

---
+++ _content/SwashbucklerDiary.Rcl/js/utility.js
@@ -0,0 +1,19 @@
+﻿function preventDefaultOnmousedown(el) {
+    if (el == null) {
+        return;
+    }
+
+    el.onmousedown = (e) => {
+        e.preventDefault();
+    }
+}
+
+function addToHead(html) {
+    const element = document.createRange().createContextualFragment(html);
+    document.getElementsByTagName('head')[0].appendChild(element);
+}
+
+function evaluateJavascript(fn) {
+    let Fn = Function;
+    return new Fn('return ' + fn)();
+}
